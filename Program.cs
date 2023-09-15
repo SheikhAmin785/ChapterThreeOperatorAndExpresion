94914544e01f411840208da8ad0c9f5d39650047
MD.AL-AMIN <143488695+SheikhAmin785@users.noreply.github.com>
2023-09-16 01:45:26 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ChapterThreeOperatorAndExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5 + 9;
+            Console.WriteLine(a);
+            int a = 5;
+            int b = 7;
+            Console.WriteLine(a + b);
+
+            bool a = true;
+            bool b = false;
+            Console.WriteLine(a && b);
+            Console.WriteLine(a || b);
+
+
+
+            int x = 10, int= 5;
+            Console.WriteLine("x> is" + (x > y));
+
+
+            Console.WriteLine(DateTime.Now);
+
+            int[] isArray = { 1, 2, 3, 4 };
+            Console.WriteLine(isArray[0]);
+
+            string str = "Hello";
+            Console.WriteLine(str[1]);
+
+
+            //implicit
+            int myInt = 5;
+            Console.WriteLine(myInt); 
+            long myLong = myInt;
+            Console.WriteLine(myLong);
+            Console.WriteLine(myLong + myInt);
+
+            //Explicit
+            double myDouble = 5.1d;
+            Console.WriteLine(myDouble); 
+            long myLong = (long)myDouble;
+            Console.WriteLine(myLong);
+
+
+
+
+            long myLong = long.MaxValue;
+            Console.WriteLine(myLong);
+            Console.WriteLine(myInt);
+
+
+            double maxHeight = heightInMeters; // Implicit
+            double minHeight = (double)heightInMeters; // Explicit
+
+        }
+    }
+}
+
+as new is
